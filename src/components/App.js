9105886a4a5beb594ff16f3a51a33fa08c457c1a
Hongyu Yang <hongyuy96@gmail.com>
2022-04-11 23:09:41 -0700

    basic form structure

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer.js';
+import Main from './Main.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/SatelliteList.js
@@ -0,0 +1,23 @@
+
+import React, {Component} from 'react';
+import { Button } from 'antd';
+
+class SatelliteList extends Component {
+    render() {
+        return (
+            <div className="sat-list-box">
+                <div className="btn-container">
+                    <Button
+                        className="sat-list-btn"
+                        size="large"
+                    >Track on the map</Button>
+                </div>
+                <hr/>
+                <div>data</div>
+            </div>
+        );
+    }
+}
+
+export default SatelliteList;
+
